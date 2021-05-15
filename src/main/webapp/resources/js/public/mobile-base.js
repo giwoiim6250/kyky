e0058d49c27e0066ea4d4d8bc3537724c9652f65
giwoiim6250 <wei19924imt@126.com>
2021-05-15 16:52:32 +0800

    commit

--- src/main/webapp/resources/js/public/mobile-base.js
+++ src/main/webapp/resources/js/public/mobile-base.js
@@ -1,10 +1,10 @@
 /**
  * mobile-base 1.0
  * Mobile terminal font size calculation
- * 
+ *
  * Copyright 2016, zhangweiming
  * http://www.surto.cn/
- * 
+ *
  * Released on: August 1, 2016
  */
 
@@ -15,7 +15,7 @@
 	function w() {
 		var r = document.documentElement;
 		var a = r.getBoundingClientRect().width;
-		a > 640 && (a = 640), rem = a/640 *100 , r.style.fontSize = rem + "px"
+		a > 1080 && (a = 1080), rem = a/1080 *100 , r.style.fontSize = rem + "px"
 	}
 
 	var t;
